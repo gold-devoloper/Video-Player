8dcfcc9d317f16ec724e3a7dd301bc3d23401c37
Lenovo <melizyayevx@gmail.com>
2022-11-06 11:34:33 +0500

    Video Player

--- widget/src/androidTest/java/gold/android/iplayer/ExampleInstrumentedTest.java
+++ widget/src/androidTest/java/gold/android/iplayer/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.android.iplayer.test", appContext.getPackageName());
+        assertEquals("gold.android.iplayer.test", appContext.getPackageName());
     }
 }
